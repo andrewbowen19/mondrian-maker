007fa4169ecbc6a25cd7e793175c2fb8ff3e1045
andrewbowen19 <atbowen@gmail.com>
2022-01-01 11:28:12 -0500

    Initial commit, setting up class and module

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "mondrian-maker",
+    version = "0.0.1",
+    author = "Andrew Bowen",
+    author_email = "atbowen@gmail.com@gmail.com",
+    description = ("Python package that produces randomly-generated Mondrian-style plots"),
+    license = "GNU General Public License v2.0",
+    keywords = "example documentation tutorial",
+    url = "https://github.com/andrewbowen19/mondrian-maker/blob/main/LICENSE",
+    packages=find_packages()
+)
