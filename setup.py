79a07e12188b9853766a7c6de6d228bc01d0e130
andrewbowen19 <atbowen@gmail.com>
2022-01-01 11:54:47 -0500

    Added setup script, testing script

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "mondrian-maker",
     version = "0.0.1",
     author = "Andrew Bowen",
-    author_email = "atbowen@gmail.com@gmail.com",
+    author_email = "atbowen@gmail.com",
     description = ("Python package that produces randomly-generated Mondrian-style plots"),
     license = "GNU General Public License v2.0",
     keywords = "example documentation tutorial",
